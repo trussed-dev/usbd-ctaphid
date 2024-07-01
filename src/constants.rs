06a734897378aecc72fa62c9f026cf7af3a801e0
Kyle Kotowick <kyle.kotowick@sbt-ext.com>
2024-07-01 17:16:27 -0400

    Revert to original calculation for max message size

--- src/constants.rs
+++ src/constants.rs
@@ -2,9 +2,5 @@ pub const INTERRUPT_POLL_MILLISECONDS: u8 = 5;
 
 pub const PACKET_SIZE: usize = 64;
 
-// 1200
-// pub const MESSAGE_SIZE: usize = ctap_types::sizes::REALISTIC_MAX_MESSAGE_SIZE;
-// pub const MESSAGE_SIZE: usize = 3072;
-// 7609 bytes is max message size for ctaphid
-// ctaphid_dispatch::types::Message.len() == 7069;
-pub const MESSAGE_SIZE: usize = 7069;
+// 7609
+pub const MESSAGE_SIZE: usize = PACKET_SIZE - 7 + 128 * (PACKET_SIZE - 5);
